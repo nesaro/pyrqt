c5e6ad1ba1237009fd7bfcb1fcde8ddbc8b53f2e
Néstor Arocha Rodríguez <nesaro@colonymbus.com>
2012-02-14 21:23:05 +0000

    translations to spanish

---
+++ driza_es_ES.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="es">
+<defaultcodec></defaultcodec>
+<context>
+    <name>InterfazQt4</name>
+    <message>
+        <location filename="Driza/iuqt4/interfaz.py" line="91"/>
+        <source>Error loading operations</source>
+        <translation type="obsolete">Error cargando operaciones</translation>
+    </message>
+</context>
+<context>
+    <name>VPrincipal</name>
+    <message>
+        <location filename="Driza/iuqt4/interfaz.py" line="91"/>
+        <source>Error loading operations</source>
+        <translation>Error cargando operaciones</translation>
+    </message>
+</context>
+<context>
+    <name>VentanaPrincipal</name>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="36"/>
+        <source>Driza</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="50"/>
+        <source>Edición</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="64"/>
+        <source>Datos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="71"/>
+        <source>Analizar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="75"/>
+        <source>Estadística Descriptiva</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="81"/>
+        <source>Medias</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="215"/>
+        <source>Ayuda</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="105"/>
+        <source>toolBar</source>
+        <translation>Barra de Herramientas</translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="125"/>
+        <source>Nuevo proyecto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="130"/>
+        <source>Abrir Proyecto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="135"/>
+        <source>Importar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="140"/>
+        <source>Guardar</source>
+        <translation type="unfinished">Miguardar</translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="145"/>
+        <source>Guardar como...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="150"/>
+        <source>Configuración</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="155"/>
+        <source>Salir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="160"/>
+        <source>Deshacer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="165"/>
+        <source>Rehacer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="170"/>
+        <source>Cortar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="175"/>
+        <source>Copiar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="180"/>
+        <source>Pegar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="185"/>
+        <source>Buscar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="190"/>
+        <source>Borrar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="195"/>
+        <source>Crear nuevas variables</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="200"/>
+        <source>Filtrar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="205"/>
+        <source>Descriptivos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="210"/>
+        <source>Media de dos muestras independientes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Driza/iuqt4/ui/vprincipal.ui" line="220"/>
+        <source>Acerca...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
