0366071d5e04beaf2a19b33eb76a8c0a7c399584
Nestor Arocha <nesaro@gmail.com>
2018-06-01 09:35:06 +0100

    some fixes

--- Driza/iuqt4/vprincipal/vprincipal.py
+++ Driza/iuqt4/vprincipal/vprincipal.py
@@ -311,7 +311,7 @@ class VPrincipal(QtGui.QMainWindow):
         """Funcion de salida del programa. Guarda la configuracion"""
         if not self.__dproyecto_modificado(): 
             return
-        self.__config.guardar()
+        self.__config.save()
         QtGui.qApp.exit(0)
 
 
