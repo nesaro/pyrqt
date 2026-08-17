f92d1d348997e527701f973863136a678ea2d57a
Nestor Arocha <n@nestorarocha.com>
2026-08-17 15:27:33 +0100

    python has pyqt available, wayland support

--- shell.nix
+++ shell.nix
@@ -1,7 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
+let
+pkgs = import <nixpkgs> {};
+python_with_deps = pkgs.python312.withPackages (p: [ p.pyqt5 p.rpy2]);
+#qt_with_wayland = pkgs.qt5.withPackages (p: [p.qtwayland]);
+in pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ python312 python312.pkgs.pyqt5 python312.pkgs.rpy2 black];
+    nativeBuildInputs = with pkgs.buildPackages; [  qt5.qtwayland python_with_deps black];
       shellHook = ''
       '';
 }
